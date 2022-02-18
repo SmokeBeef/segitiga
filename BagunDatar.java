f49f725556b260fa7a5cc9aff981f494bfd722fe
SmokeBeef <99931032+SmokeBeef@users.noreply.github.com>
2022-02-18 08:29:40 +0700

    Add files via upload

---
+++ BagunDatar.java
@@ -0,0 +1,21 @@
+package bagundatar;
+
+
+public class BagunDatar {
+
+    double luas;
+    double a = 0.5;
+    
+    void luas (double panjang, double lebar){
+        luas = panjang*lebar;
+        System.out.println("Luas pertama = "+ luas);
+    
+    }
+    
+    void luas (int panjang, int lebar, int a){
+        luas = panjang *lebar*a;
+        System.out.println("Luas kedua = "+ luas);
+    
+    }
+    
+}
---
+++ menghitung.java
@@ -0,0 +1,13 @@
+package bagundatar;
+
+
+public class menghitung {
+    
+    public static void main(String[] args) {
+        BagunDatar hitung = new BagunDatar();
+        hitung.luas(98,23);
+        hitung.luas(9,3,5);
+        
+    }
+    
+}
